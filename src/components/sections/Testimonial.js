05eabcc94828ef64d1dfb7574d24066f85c1d950
alessandrocapanna <acapanna75@gmail.com>
2020-12-08 00:20:30 +0100

    prova

---
+++ src/components/sections/Testimonial.js
@@ -0,0 +1,114 @@
+import React, { useState, useRef, useEffect } from 'react';
+import classNames from 'classnames';
+import { SectionTilesProps } from '../../utils/SectionProps';
+import SectionHeader from './partials/SectionHeader';
+
+
+const propTypes = {
+  ...SectionTilesProps.types
+}
+
+const defaultProps = {
+  ...SectionTilesProps.defaults
+}
+
+const Testimonial = ({
+  className,
+  topOuterDivider,
+  bottomOuterDivider,
+  topDivider,
+  bottomDivider,
+  hasBgColor,
+  invertColor,
+  pushLeft,
+  ...props
+}) => {
+
+  const outerClasses = classNames(
+    'testimonial section',
+    topOuterDivider && 'has-top-divider',
+    bottomOuterDivider && 'has-bottom-divider',
+    hasBgColor && 'has-bg-color',
+    invertColor && 'invert-color',
+    className
+  );
+
+  const innerClasses = classNames(
+    'testimonial-inner section-inner',
+    topDivider && 'has-top-divider',
+    bottomDivider && 'has-bottom-divider'
+  );
+
+  const tilesClasses = classNames(
+    'tiles-wrap',
+    pushLeft && 'push-left'
+  );
+
+  const sectionHeader = {
+    title: 'Testimonianze',
+    paragraph: 'Ecco alcuni dei nostri clienti pienamente soddisfatti.'
+  };
+  
+  return (
+    <section
+      {...props}
+      className={outerClasses}
+    >
+      <div className="container" >
+        <div className={innerClasses}>
+          <SectionHeader data={sectionHeader} className="center-content" />
+          <div className={tilesClasses}>
+
+            <div className="tiles-item reveal-from-right" data-reveal-delay="200">
+              <div className="tiles-item-inner">
+                <div className="testimonial-item-content">
+                  <p className="text-sm mb-0">
+                    — Dopo aver provato velop non sono più tornato indietro,è unico nel suo genere e offre un servizio a un prezzo super competitivo!.
+                      </p>
+                </div>
+                <div className="testimonial-item-footer text-xs mt-32 mb-0 has-top-divider">
+                  <span className="testimonial-item-name text-color-high">Mirko Scarcella</span>
+
+                </div>
+              </div>
+            </div>
+
+            <div className="tiles-item reveal-from-right" data-reveal-delay="200">
+              <div className="tiles-item-inner">
+                <div className="testimonial-item-content">
+                  <p className="text-sm mb-0">
+                    — Dopo aver provato velop non sono più tornato indietro,è unico nel suo genere e offre un servizio a un prezzo super competitivo!.
+                      </p>
+                </div>
+                <div className="testimonial-item-footer text-xs mt-32 mb-0 has-top-divider">
+                  <span className="testimonial-item-name text-color-high">Mirko Scarcella</span>
+
+                </div>
+              </div>
+            </div>
+
+            <div className="tiles-item reveal-from-right" data-reveal-delay="200">
+              <div className="tiles-item-inner">
+                <div className="testimonial-item-content">
+                  <p className="text-sm mb-0">
+                    — Dopo aver provato velop non sono più tornato indietro,è unico nel suo genere e offre un servizio a un prezzo super competitivo!.
+                      </p>
+                </div>
+                <div className="testimonial-item-footer text-xs mt-32 mb-0 has-top-divider">
+                  <span className="testimonial-item-name text-color-high">Mirko Scarcella</span>
+
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+Testimonial.propTypes = propTypes;
+Testimonial.defaultProps = defaultProps;
+
+export default Testimonial;
