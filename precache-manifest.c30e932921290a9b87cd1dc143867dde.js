9c68f82392dda0baca8184abb398c03950a9c396
alessandrocapanna <acapanna75@gmail.com>
2020-12-08 01:10:50 +0100

    Updates

---
+++ precache-manifest.c30e932921290a9b87cd1dc143867dde.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7a911974c7183c0e69aed155e873ee9",
+    "url": "/alessandrocapanna/mattia/index.html"
+  },
+  {
+    "revision": "c1889769e36ae9d51ebe",
+    "url": "/alessandrocapanna/mattia/static/css/main.ea9507ce.chunk.css"
+  },
+  {
+    "revision": "054bac9e7361800b6998",
+    "url": "/alessandrocapanna/mattia/static/js/2.769e1417.chunk.js"
+  },
+  {
+    "revision": "ce15a0ffcfe8b91e4f219c1d7bb8c5ef",
+    "url": "/alessandrocapanna/mattia/static/js/2.769e1417.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1889769e36ae9d51ebe",
+    "url": "/alessandrocapanna/mattia/static/js/main.f6981aaa.chunk.js"
+  },
+  {
+    "revision": "ed010f5edaba22215273",
+    "url": "/alessandrocapanna/mattia/static/js/runtime-main.df6a6373.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/alessandrocapanna/mattia/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/alessandrocapanna/mattia/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/alessandrocapanna/mattia/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/alessandrocapanna/mattia/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "/alessandrocapanna/mattia/static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "d9cb99ceb6a66250792ed78f5765325b",
+    "url": "/alessandrocapanna/mattia/static/media/features-split-image-01.d9cb99ce.png"
+  },
+  {
+    "revision": "199e9ec14308832c6a8a49bd96c1fde6",
+    "url": "/alessandrocapanna/mattia/static/media/hero.199e9ec1.jpg"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/alessandrocapanna/mattia/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/alessandrocapanna/mattia/static/media/illustration-section-02.63d0555d.svg"
+  }
+]);
